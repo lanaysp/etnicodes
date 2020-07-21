d1ca5dfd8a54613cd4b8080e59ab3ccd757f2483
lanaysp <lanaysps@gmail.com>
2020-07-21 18:42:37 +0700

    upload

---
+++ application/modules/layout/views/navbar.php
@@ -0,0 +1,30 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+        <div class="logo mr-auto">
+            <h1 class="text-light">
+                <a href="index.html"><span><img src="<?= config_item('img') ?>/logo/l_big.png" alt="" /></span></a>
+            </h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        </div>
+
+        <nav class="nav-menu d-none d-lg-block">
+            <ul>
+                <li class="active"><a href="#index.html">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#team">Team</a></li>
+                <li><a href="#pricing">Pricing</a></li>
+                <li class="drop-down"></li>
+
+                <li><a href="#contact">Contact</a></li>
+
+                <li class="get-started"><a href="#about">Get Started</a></li>
+            </ul>
+        </nav>
+        <!-- .nav-menu -->
+    </div>
+</header>
+<!-- End Header -->
